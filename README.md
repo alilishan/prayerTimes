20cdea362ece8088173fb1d1eb89bc2864d21a58
alilishan <alilishan@gmail.com>
2015-12-04 21:43:27 +0800

    Readme Updated

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # prayerTimes
+
+Prayer Times Calculations based on:
+
+http://praytimes.org/wiki/Code
+http://praytimes.org/wiki/Code_Manual
+http://praytimes.org/code/git/?a=tree&p=PrayTimes&hb=HEAD&f=v2/js
+http://praytimes.org/calculation/
+
+Hijri Date/Time conversions:
+
+https://github.com/xsoh/moment-hijri
+
+Date/Time Calcultations
+
+http://stackoverflow.com/questions/18623783/get-the-time-difference-between-two-datetimes
+http://stackoverflow.com/questions/30668373/moment-js-test-if-a-date-is-today-yesterday-within-a-week-or-two-weeks-ago
+
--- index.php
+++ index.php
@@ -22,14 +22,6 @@
 
 	$(document).ready(function(){
 
-		//http://praytimes.org/wiki/Code
-		//http://praytimes.org/wiki/Code_Manual
-		//http://praytimes.org/code/git/?a=tree&p=PrayTimes&hb=HEAD&f=v2/js
-		//http://praytimes.org/calculation/
-		//https://github.com/xsoh/moment-hijri
-		//http://stackoverflow.com/questions/18623783/get-the-time-difference-between-two-datetimes
-		//http://stackoverflow.com/questions/30668373/moment-js-test-if-a-date-is-today-yesterday-within-a-week-or-two-weeks-ago
-
 		//navigator.geolocation.getCurrentPosition(showPosition);
 
 		var pTIMES = prayTimes.getTimes(new Date(), [LATITUDE, LONGITUDE], TIMEZONE);
